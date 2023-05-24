b8373ae9bffb15cd17a0782da87dc5ec413366c1
Marii lopez <lopezmariana997@gmail.com>
2023-05-23 19:19:59 -0500

    funcion de extraer links de los archivos

--- Api.js
+++ Api.js
@@ -1,17 +1,26 @@
+import { error } from 'node:console';
 import { readFile } from 'node:fs';
 
 //Solo leer los archivos
-export const leerArchivo = () => {
-  return new Promise ((resolve, reject)=>{
-    readFile('Readme.md', 'utf8', (err, data) => {
-    if (err) reject(err);
-    {
-      // console.log('algo pasa aqui', data)
-       resolve(data)
-    }
+export const leerArchivo = (route) => {
+  return new Promise((resolve, reject) => {
+    readFile(route, 'utf8', (err, data) => {
+      if (err) reject(err);
+      {
+        // console.log('algo pasa aqui', data)
+        resolve(data)
+      }
+    })
   })
- })
-} 
- 
+}
 //Extraer los links 
+export const extraerLinks = (contenido) => {
 
+  let regex = /(?=\[(!\[.+?\]\(.+?\)|.+?)]\((https:\/\/[^\)]+)\))/gi
+
+  let links = [...contenido.matchAll(regex)].map((m) => ({ text: m[1], link: m[2] }))
+
+  console.log('aqui pasa algo', links)
+  return links
+}
+//Hacer peticion HTTP
--- index.js
+++ index.js
@@ -3,52 +3,54 @@ import { log } from 'console';
 import { existsSync, statSync } from 'fs';
 // AS es para crear un alias cuando dos nombres son los mismos y no se crucen en la funcion 
 import { isAbsolute, resolve as resolvePath, extname } from 'path';
-import { leerArchivo } from "./Api.js";
+import { extraerLinks, leerArchivo } from "./Api.js";
 // Mi constante debe tener como parametro Path y Option (lo que necesitamos reconocer y como resolverlo(si valida o no))
 //const mdLinksMl = (path, options) => {
-export const mdLinksMl = (path = "README.md", options) => { // Creamos la instancia de markdown-it
+export const mdLinksMl = (route, options) => { // Creamos la instancia de markdown-it
   //Creamos la promesa, Funtion(Ejecuta), Callback(resolve y reject = funcion que resuelve la promesa(.then) o rechasa la promesa(.catch))
   return new Promise((resolve, reject) => {
     //Identificamos si es una ruta 
-    if (existsSync(path)) {
+    if (existsSync(route)) {
       console.log('la ruta existe')
-//confirmamos si la ruta es absoluta o la convertimos 
-// startsWith()Determina si una cadena comienza con los caracteres dde una cadena especifica
-const isPathAbsolute = isAbsolute(path);
-if(isPathAbsolute === true){
-  console.log('Ruta absoluta');
-  //Nuestra ruta absoluta
-  //usar "statSync()" es un metodo estatico de fs 
-  // let stats = statSync(path);
-  // if(stats.isFile() === true){
-  //   console.log('es un archivo', extname(path));
-  //   //creamos la instancia del archivo 
-  //   if(extname(path) === ".md"){
-  //     console.log('Es un archivo MD');
-  //   }else{
-  //     console.log('Este archivo no contiene MD');
-  //   }
-  // }
-}else{
-  console.log('ruta resolviendose', resolvePath(path));
-  // convertir la ruta
-  let stats = statSync(path);
-if(stats.isFile() === true){
-  console.log('es un archivo', extname(path));
-  //creamos la instancia del archivo 
-  if(extname(path) === ".md"){
-    console.log('Es un archivo MD');
-    leerArchivo().then((res)=>{ console.log(res);})
-  }else{
-    console.log('Este archivo no contiene MD');
-  }
-}else{
-  console.log('Por el momento no leemos directorios, prueba con un archivo');
-}
-}
-//Contiua la logica de Diagrama 
+      //confirmamos si la ruta es absoluta o la convertimos 
+      // startsWith()Determina si una cadena comienza con los caracteres dde una cadena especifica
+      //const isPathAbsolute = isAbsolute(route);
+      if (isAbsolute(route)) {
+        console.log('Ruta absoluta');
+        //Nuestra ruta absoluta
+        //usar "statSync()" es un metodo estatico de fs 
+        // let stats = statSync(path);
+        // if(stats.isFile() === true){
+        //   console.log('es un archivo', extname(path));
+        //   //creamos la instancia del archivo 
+        //   if(extname(path) === ".md"){
+        //     console.log('Es un archivo MD');
+        //   }else{
+        //     console.log('Este archivo no contiene MD');
+        //   }
+        // }
+      } else {
+        console.log('ruta resolviendose', resolvePath(route));
+        // convertir la ruta
+        let stats = statSync(route);
+        if (stats.isFile() === true) {
+          console.log('es un archivo', extname(route));
+          //creamos la instancia del archivo 
+          if (extname(route) === ".md") {
+            console.log('Es un archivo MD');
+            leerArchivo(route).then((res) => {
+              console.log(res, extraerLinks(res));
+            })
+          } else {
+            console.log('Este archivo no contiene MD');
+          }
+        } else {
+          console.log('Por el momento no leemos directorios, prueba con un archivo');
+        }
+      }
+      //Contiua la logica de Diagrama 
 
-//comprobamos si es un archivo ("statSync" Permite saber si el archivo encontrado es un directorio o una funcion)
+      //comprobamos si es un archivo ("statSync" Permite saber si el archivo encontrado es un directorio o una funcion)
     } else {
       //Si no existe la ruta rechazamos la promesa.
       reject('La ruta no existe, no podemos continuar')
@@ -64,6 +66,6 @@ if(stats.isFile() === true){
 // Import the path module
 // const path = require('path');
 
-   
 
-mdLinksMl();
+
+mdLinksMl('README.md');
